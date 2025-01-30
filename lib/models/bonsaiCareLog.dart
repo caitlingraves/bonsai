94b3c53e15bde1d2039707c3bb87217f540d24d1
caitlingraves <cgraveshna21@outlook.com>
2025-01-29 16:22:37 -0800

    add main dart app functionality

---
+++ lib/models/bonsaiCareLog.dart
@@ -0,0 +1,13 @@
+// lib/models/bonsai_care_log.dart
+
+class BonsaiCareLog {
+  final DateTime date;
+  final String activity;
+  final String notes;
+
+  BonsaiCareLog({
+    required this.date,
+    required this.activity,
+    required this.notes,
+  });
+}
